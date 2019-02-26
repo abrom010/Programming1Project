cf524de6ecff75cc868c0ebf73e918bfbddd3e4d
abrom010 <42874694+abrom010@users.noreply.github.com>
2019-02-26 15:55:21 -0500

    Update project.java

--- project.java
+++ project.java
@@ -1,3 +1,18 @@
+/**
+Team Leader: Aaron Brombergues, 6131735, U01
+Student:
+Student:
+
+4 - Exercise 7.7 of section Programming Projects in Chapter 7, page 366 of our textbook.
+
+or
+
+6 - Write a program that simulates a statistics tool, by letting the user determine the minimum, the
+maximum, the range (maximum – minimum), the mode (value that is repeated more often), the mean
+(average), and the standard deviation of a set of numbers. These numbers, that will be stored in an
+array, represent salaries in the $30K – $60K range and are to be generated randomly.
+*/
+
 public class project{
   public static void main(String[] args){
   
