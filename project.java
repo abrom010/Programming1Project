c20bba2232ec3ec59041e9e4e0ed2dd3cf002daa
abrom010 <42874694+abrom010@users.noreply.github.com>
2019-02-26 21:17:03 -0500

    Update project.java

--- project.java
+++ project.java
@@ -9,7 +9,7 @@ maximum, the range (maximum – minimum), the mode (value that is repeated more 
 array, represent salaries in the $30K – $60K range and are to be generated randomly.
 This is the definition of standard deviation,
 
-sqrt[ (Σ(x-x̄)^2)/(n-1) ]  //sum from i=1 to n
+sqrt[ (Σ(x_i -x̄)^2)/(n-1) ]  //sum from i=1 to n
 
 where x̄ denotes the average, and n is the total number of elements in the array.
 The program will present the user a menu with these choices:
@@ -40,42 +40,141 @@ public class project{
             
         System.out.println("Please type in a choice (spelled correctly) then hit enter.");
         
-        System.out.println("To show this menu again, type in \"Menu\".");
+        System.out.println("To show this menu again, type in \"Menu\".\n");
     }
     
-    //Creates a randomized salary array with 1 user input (arrayLength)
-    static double[] createRandomSalary(){
+    //Creates a randomized salary array (30k-60k)
+    static float[] createRandomSalary(int arrayLength){
         Scanner in = new Scanner(System.in);
-        System.out.println("How many salaries in the array?");
-        int arrayLength = in.nextInt();
         
-        double[] salaries = new double[arrayLength];
+        float[] salaries = new float[arrayLength];
 
         for(int i=0;i<arrayLength;i++){
-            salaries[i]= 30000+Math.random()*30000;
+            salaries[i]= 30000+(float)Math.random()*30000;
         }
         return salaries;
       }
+    
+    //Gets a minimum
+    static float getMinimum(float[] salaryArray){
+        float min=60000;
+        for(int i=0;i<salaryArray.length;i++){
+            min =salaryArray[i]<min?salaryArray[i]:min;
+        }
+        return min;
+    }
+    
+    //Gets a maximum
+    static float getMaximum(float[] salaryArray){
+        float max=30000;
+        for(int i=0;i<salaryArray.length;i++){
+            max =salaryArray[i]>max?salaryArray[i]:max;
+        }
+        return max;
+    }
+    
+    //Gets a mode. If no mode, outputs 0
+    static float getMode(float[] salaryArray){
+        float mode = 0;
+        int maxCount = 0;
+        for (int i=0;i<salaryArray.length;i++){
+            float value = salaryArray[i];
+            int count = 0;
+            for (int j=0;j<salaryArray.length;j++){
+                if (salaryArray[j] == value) count++;
+                if (count>maxCount){
+                    mode = value;
+                    maxCount = count;
+                if(count==1){
+                    mode = 0;
+        }}}}
+        return mode;
+    }
+    
+    //Gets an average
+    static float getAverage(float[] salaryArray){
+        float sum = 0;
+        for(int i=0;i<salaryArray.length;i++){
+            sum+=salaryArray[i];
+        }
+        return sum/salaryArray.length;
+    }
+    
+    //Gets standard deviation
+    static float getStandardDeviation(float[] salaryArray){
+        float sum=0;
+        float standardDeviation=0;
+        for(int i=0;i<salaryArray.length;i++){
+            sum+=(float)Math.pow(salaryArray[i]-getAverage(salaryArray), 2);
+            standardDeviation = (float)Math.sqrt(sum/(salaryArray.length-1));
+        }
+        return standardDeviation;
+    }
 
     //Main method
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         boolean exit = false;
+        boolean populated = false;
+        
+        //Getting an array length from user
+        System.out.println("An array must be initialized. How many salaries would you like to compare?");
+        int arrayLength = in.nextInt();
+        float[] salaryArray = new float[arrayLength];
+        
         displayMenu();
         
         //Main loop
         while(!exit){
             System.out.print(": ");
-            String choice = in.nextLine();
+            String choice = input.nextLine();
             
             switch(choice.toUpperCase()){
-                case "POPULATE ARRAY": break;
-                case "DISPLAY ARRAY": break;
-                case "MINIMUM": break;
-                case "RANGE": break;
-                case "MODE": break;
-                case "MEAN": break;
-                case "STANDARD DEVIATION": break;
+                case "POPULATE ARRAY":
+                    salaryArray = createRandomSalary(arrayLength);
+                    populated = true;
+                    break;
+                    
+                case "DISPLAY ARRAY":
+                    if(populated)
+                    {
+                        System.out.println(Arrays.toString(salaryArray));
+                        break;
+                    }
+                    else
+                    {
+                        System.out.println("Please populate the array first.");
+                        break;
+                    }
+                    
+                case "MINIMUM":
+                    System.out.println("Minimum is: "+getMinimum(salaryArray));
+                    break;
+                    
+                case "MAXIMUM":
+                    System.out.println("Maximum is: "+getMaximum(salaryArray));
+                    break;
+                    
+                case "RANGE":
+                    System.out.println("Range is: "+(getMaximum(salaryArray)-getMinimum(salaryArray)));
+                    break;
+                    
+                case "MODE":
+                    if(getMode(salaryArray)!=0){
+                    System.out.println("Mode is: "+getMode(salaryArray));
+                    }
+                    else System.out.println("No mode.");
+                    break;
+                    
+                case "MEAN":
+                    System.out.println("Mean is: "+getAverage(salaryArray));
+                    break;
+                    
+                case "STANDARD DEVIATION":
+                    System.out.println("Standard deviation is: "+getStandardDeviation(salaryArray));
+                    break;
+                
                 case "MENU": displayMenu(); break;
                 case "EXIT": exit=true; break;
                 default: System.out.println("Please give valid input");
